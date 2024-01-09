d04781fa7cb42745a750b9212497b8e76b766abf
Bote Schaafsma <bschaafs@f1r1s16.codam.nl>
2024-01-09 17:04:38 +0100

    update

--- makerc/definitions.mk
+++ makerc/definitions.mk
@@ -36,7 +36,12 @@ SRCS			:=$(wildcard $(SRC_DIR)/*.c) \
 						$(wildcard $(SRC_DIR)/error/*.c) \
 						$(wildcard $(SRC_DIR)/parse/*.c) \
 						$(wildcard $(SRC_DIR)/rgb/*.c) \
-						$(wildcard $(SRC_DIR)/scene/*.c)
+						$(wildcard $(SRC_DIR)/scene/*.c) \
+						$(wildcard $(SRC_DIR)/init/*.c) \
+						$(wildcard $(SRC_DIR)/dda/*.c) \
+						$(wildcard $(SRC_DIR)/utils/*.c) \
+						$(wildcard $(SRC_DIR)/move_player/*.c)
+
 HEADERS			:=$(INCLUDE_DIR)/*.h \
 					$(LIBFT_DIR)/include/*.h \
 					$(MLX42_DIR)/include/*.h
